04cdaf2701f919acb252a7084bbcb511a29c1ab1
Wakype <hilmim152bisnis@gmail.com>
2023-01-21 13:13:13 +0700

    [21/01/2023] Home Screen Complete

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,8 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, non_constant_identifier_names, use_key_in_widget_constructors, unused_local_variable, annotate_overrides
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables, non_constant_identifier_names, use_key_in_widget_constructors, unused_local_variable, annotate_overrides, sized_box_for_whitespace
 
 // import 'dart:html';
+// import 'dart:html';
+import 'dart:ffi';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -309,7 +311,6 @@ class HomeView extends GetView<HomeController> {
                         ),
                       ),
                       Container(
-                        alignment: Alignment.centerLeft,
                         child: SingleChildScrollView(
                             scrollDirection: Axis.horizontal,
                             child: Row(
@@ -341,7 +342,7 @@ class HomeView extends GetView<HomeController> {
                   child: Column(
                     children: [
                       Container(
-                        padding: EdgeInsets.fromLTRB(25, 0, 25, 16),
+                        padding: EdgeInsets.fromLTRB(25, 0, 25, 12),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -365,11 +366,179 @@ class HomeView extends GetView<HomeController> {
                         ),
                       ),
                       Container(
-                        child: Row(
-                          children: [],
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Container(
+                            height: 340,
+                            child: Row(
+                              children: [
+                                SizedBox(width: 25),
+                                ProdukCard(
+                                    produk: 'Logitech G603 Lightspeed High DPI',
+                                    daerah: 'Kab. Bandung',
+                                    diskon: '44%',
+                                    gambar: 'assets/images/mouse.png',
+                                    harga: 'Rp 609.000',
+                                    potongan: 'Rp 1.000.000',
+                                    rating: '4.8',
+                                    terjual: '312'),
+                                ProdukCard(
+                                    produk: 'Logitech G203 Mouse Gaming',
+                                    daerah: 'Kab. Bandung',
+                                    diskon: '46%',
+                                    gambar: 'assets/images/mouseBanyak.png',
+                                    harga: 'Rp 204.000',
+                                    potongan: 'Rp 379.000',
+                                    rating: '4.9',
+                                    terjual: '6rb'),
+                                ProdukCard(
+                                    produk: 'Logitech G603 Lightspeed High DPI',
+                                    daerah: 'Kab. Bandung',
+                                    diskon: '44%',
+                                    gambar: 'assets/images/mouse.png',
+                                    harga: 'Rp 609.000',
+                                    potongan: 'Rp 1.000.000',
+                                    rating: '4.8',
+                                    terjual: '312'),
+                                ProdukCard(
+                                    produk: 'Logitech G203 Mouse Gaming',
+                                    daerah: 'Kab. Bandung',
+                                    diskon: '46%',
+                                    gambar: 'assets/images/mouseBanyak.png',
+                                    harga: 'Rp 204.000',
+                                    potongan: 'Rp 379.000',
+                                    rating: '4.9',
+                                    terjual: '6rb'),
+                              ],
+                            ),
+                          ),
                         ),
                       )
                     ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 25, 0, 25),
+                  width: lebar,
+                  height: 8,
+                  color: Color(0xffeeeeee),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 24),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 25,
+                        ),
+                        CatalogContainer(
+                            text: 'For Hilmi',
+                            gradient1: 0xffFA38B1,
+                            gradient2: 0xffA92176,
+                            widget: Container(
+                              width: 25,
+                              height: 3,
+                              margin: EdgeInsets.only(bottom: 5),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(2),
+                                  color: Colors.white),
+                            )),
+                        CatalogContainer(
+                            text: 'Special Discount',
+                            gradient1: 0xff8A40FF,
+                            gradient2: 0xff462180,
+                            widget: Container()),
+                        CatalogContainer(
+                            text: 'Aktifitasmu',
+                            gradient1: 0xff3BD5FF,
+                            gradient2: 0xff0C87A9,
+                            widget: Container()),
+                        CatalogContainer(
+                            text: 'Gaming Setups',
+                            gradient1: 0xffFFBE53,
+                            gradient2: 0xffEE9B12,
+                            widget: Container()),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 24),
+                  child: Wrap(spacing: 15, runSpacing: 20, children: [
+                    ProdukCard(
+                        gambar: 'assets/images/steelseries.png',
+                        daerah: 'Jakarta Pusat',
+                        diskon: '12%',
+                        harga: 'Rp 699.000',
+                        potongan: 'Rp 790.000',
+                        produk: 'SteelSeries Rival 3 Wireless - Gaming ...',
+                        rating: '5.0',
+                        tinggi: 340,
+                        lebar: 165,
+                        tinggiGambar: 165,
+                        lebarGambar: 165,
+                        marginKanan: 0,
+                        terjual: '124'),
+                    ProdukCard(
+                        gambar: 'assets/images/monitor.png',
+                        daerah: 'Kota Depok',
+                        diskon: '44%',
+                        harga: 'Rp 5.949.900',
+                        potongan: 'Rp 1.000.000',
+                        produk: 'Monitor Lenovo G34W-30 34" ...',
+                        rating: '5.0',
+                        tinggi: 340,
+                        lebar: 165,
+                        tinggiGambar: 165,
+                        lebarGambar: 165,
+                        marginKanan: 0,
+                        terjual: '23'),
+                    ProdukCard(
+                        gambar: 'assets/images/colokan.png',
+                        daerah: 'Kab. Tangerang',
+                        diskon: '44%',
+                        harga: 'Rp 3.750',
+                        potongan: 'Rp 1.000.000',
+                        produk: 'Myvo Steker T Multi Lampu Colokan ...',
+                        rating: '5.0',
+                        tinggi: 340,
+                        lebar: 165,
+                        tinggiGambar: 165,
+                        lebarGambar: 165,
+                        marginKanan: 0,
+                        terjual: '76'),
+                    ProdukCard(
+                        gambar: 'assets/images/logitech.png',
+                        daerah: 'Kab. Bandung',
+                        diskon: '44%',
+                        harga: 'Rp 609.000',
+                        potongan: 'Rp 1.000.000',
+                        produk: 'Logitech G PRO X SUPERLIGHT ...',
+                        rating: '5.0',
+                        tinggi: 340,
+                        lebar: 165,
+                        tinggiGambar: 165,
+                        lebarGambar: 165,
+                        marginKanan: 0,
+                        terjual: '1rb'),
+                  ]),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(25, 0, 25, 25),
+                  width: lebar,
+                  height: 40,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      border: Border.all(width: 1, color: abuAbu)),
+                  child: InkWell(
+                    child: Center(
+                      child: Text(
+                        'Lihat Selebihnya',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
                   ),
                 )
               ],
@@ -379,6 +548,39 @@ class HomeView extends GetView<HomeController> {
       ),
     );
   }
+}
+
+Widget CatalogContainer({text, gradient1, gradient2, widget}) {
+  return Container(
+    margin: EdgeInsets.only(right: 12),
+    padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+    width: 114,
+    height: 66,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Color(gradient1),
+            Color(gradient2),
+          ],
+        )),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        widget,
+        Container(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: 14, fontWeight: FontWeight.w600, color: Colors.white),
+          ),
+        )
+      ],
+    ),
+  );
 }
 
 Widget CustomIcon({icon, text}) {
@@ -522,6 +724,152 @@ Widget KejarDiskonCard(
                         fontSize: 10,
                         fontWeight: FontWeight.w500,
                         color: abuText)),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget ProdukCard(
+    {gambar,
+    harga,
+    diskon,
+    potongan,
+    daerah,
+    terjual,
+    rating,
+    produk,
+    double lebar = 146,
+    double tinggi = 316,
+    double lebarGambar = 146,
+    double tinggiGambar = 146,
+    double marginKanan = 15}) {
+  String truncate(String text, {length = 7, omission = '...'}) {
+    if (length >= text.length) {
+      return text;
+    }
+    return text.replaceRange(length, text.length, omission);
+  }
+
+// void main() {
+//   print(truncate('Hello, World!', length: 4));
+// }
+  return Container(
+    margin: EdgeInsets.only(right: marginKanan),
+    // padding: EdgeInsets.symmetric(vertical: 10),
+    width: lebar,
+    height: tinggi,
+    decoration: BoxDecoration(boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 0), // changes position of shadow
+      ),
+    ], borderRadius: BorderRadius.circular(8), color: Colors.white),
+    child: Column(
+      children: [
+        Container(
+          width: lebarGambar,
+          height: tinggiGambar,
+          child: Image.asset(gambar),
+        ),
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                alignment: Alignment.centerLeft,
+                child: Text(truncate(produk, length: 25),
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  harga,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 6, top: 6),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 6),
+                      width: 36,
+                      height: 20,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadiusDirectional.circular(2),
+                          color: merahTrans),
+                      child: Center(
+                        child: Text(
+                          diskon,
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                              color: merah),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        potongan,
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            decoration: TextDecoration.lineThrough,
+                            color: abuText),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 16),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(right: 2),
+                      child: Image.asset('assets/images/merchant.png'),
+                    ),
+                    Container(
+                      child: Text(
+                        daerah,
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: abuText),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(right: 4),
+                        child: Icon(
+                          CupertinoIcons.star_fill,
+                          color: Color(0xffffc400),
+                          size: 13,
+                        )),
+                    Container(
+                      child: Text('$rating | Terjual $terjual',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w400,
+                              color: abuText)),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
